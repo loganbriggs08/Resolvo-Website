c51af629c48791e30b9c178c370ff0686493b081
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2024-02-10 12:59:18 +0000

    Make some metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import NavBar from "./components/navbar";
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Solvio - Problems Made Easy.",
+  title: "Resolvo - Resolve Today, Innovate Tomorrow.",
   description: "Crafting tailored software solutions to meet the end users needs.",
   themeColor: "#DD1546",
 
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     
     title: "Resolvo - Resolve Today, Innovate Tomorrow.",
     description: "Crafting tailored software solutions to meet the end users needs.",
-    images: ["app_icon.png"]
+    images: ["logo.png"]
   }
 }
 
@@ -36,10 +36,10 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
         <meta name="theme-color" content="#DD1546"></meta>
 
-        <meta property="og:site_name" content="Solvio"></meta>
-        <meta property="og:title" content="Solvio - Problems Made Easy."></meta>
-        <meta property="og:description" content="Why should Problems have to be difficult? Don't struggle, use Solvio to remove the complexity from your homework."></meta>
-        <meta property="og:image" content="https://solvio.app/website_screenshot.png"></meta>
+        <meta property="og:site_name" content="Resolvo"></meta>
+        <meta property="og:title" content="Resolvo - Resolve Today, Innovate Tomorrow."></meta>
+        <meta property="og:description" content="Crafting tailored software solutions to meet the end users needs."></meta>
+        <meta property="og:image" content="https://resolvo.dev/website_screenshot.png"></meta>
 
         <title>Solvio - Problems Made Easy.</title>
       </head>
