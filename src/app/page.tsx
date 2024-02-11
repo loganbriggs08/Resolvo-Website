41021fd67506fcf50746cbf015c91f06f5737779
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2024-02-11 13:59:27 +0000

    Update Projects card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,41 +55,45 @@ export default function Home() {
 
         <div className={styles.bottom_section}>
           <div className={styles.examples_section}>
-            <div className={styles.inside_components}>
-              <h1 className={styles.header_text}>PROJECTS<a className={styles.white_dot}>.</a></h1>
-              <p className={styles.description_text}>Our previous & current projects.</p>
-              
-              <div className={styles.header_seperator}></div>
+            <h1 className={styles.header_text}>PROJECTS<a className={styles.white_dot}>.</a></h1>
+            <p className={styles.description_text}>Our previous & current projects.</p>
+            <div className={styles.header_seperator}></div>
 
+            <div className={styles.inside_components}>
               <div className={styles.solvio}>
                 <div className={styles.example_top_section}>
-                  <h1 className={styles.solvio_example_text}>SOLVIO.APP</h1>
-                  <p className={styles.default_medium_text}>Why should Problems have to be difficult?</p>
-
-                  <div className={styles.tags}>
-                    <div className={styles.tag}>
-                      <p className={styles.default_medium_text}>React Native</p>
-                    </div>
-
-                    <div className={styles.tag}>
-                      <p className={styles.default_medium_text}>NextJS</p>
-                    </div>
-
-                    <div className={styles.tag}>
-                      <p className={styles.default_medium_text}>Typescript</p>
-                    </div>
-
-                    <div className={styles.tag}>
-                      <p className={styles.default_medium_text}>GoLang</p>
-                    </div>
-
-                    <div className={styles.tag}>
-                      <p className={styles.default_medium_text}>Postgres</p>
-                    </div>
-                  </div>
-
                   <div className={styles.image_container}>
-                    <img src="solvio_website.png" className={styles.solvio_app_icon}/>
+                    <img src="solvio_app_icon.png" className={styles.solvio_app_icon}/>
+                  </div>
+                  <div className={styles.example_details}>
+                    <h1 className={styles.solvio_example_text}>SOLVIO.APP</h1>
+                    <p className={styles.default_medium_text}>Why should Problems have to be difficult?</p>
+                    <div className={styles.tags}>
+                      <div className={styles.tag}>
+                        <p className={styles.default_medium_text}>Expo</p>
+                      </div>
+                      <div className={styles.tag}>
+                        <p className={styles.default_medium_text}>NextJS</p>
+                      </div>
+                      <div className={styles.tag}>
+                        <p className={styles.default_medium_text}>Typescript</p>
+                      </div>
+                      <div className={styles.tag}>
+                        <p className={styles.default_medium_text}>GoLang</p>
+                      </div>
+                      <div className={styles.tag}>
+                        <p className={styles.default_medium_text}>Postgres</p>
+                      </div>
+                    </div>
+                    <div className={styles.example_section_buttons}>
+                      <a className={styles.solvio_view_website_button} href='https://solvio.app/' target='_blank'>
+                        <p className={styles.a_tag_text}>View Website</p>
+                      </a>
+                      <div className={styles.button_seperator}></div>
+                      <a className={styles.solvio_other_button} href='https://github.com/GetSolvio' target='_blank'>
+                        <p className={styles.a_tag_text}>View Github</p>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
