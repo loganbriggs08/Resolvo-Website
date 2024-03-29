9cfdb7be0c47e2afa4bc653d8ca9a5900a03a4a1
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2024-03-29 17:36:11 +0000

    fix up design a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,16 @@ import { IoIosArrowDown } from "react-icons/io";
 export default function Home() {
   const words: string[] = ["MOBILE APPS", "DESKTOP APPS", "WEB-BASED APPS"];
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(true); // Initially set to true to trigger animations
+  const [isAnimating, setIsAnimating] = useState(true); 
 
   useEffect(() => {
-    // Trigger animation by setting isAnimating to false after a delay
     const timeout = setTimeout(() => {
       setIsAnimating(false);
-    }, 1000); // Adjust this value according to your animation duration
+    }, 1000); 
 
-    // Clear the timeout to avoid memory leaks
     return () => clearTimeout(timeout);
   }, []);
 
-  // Rotate words every 2000 milliseconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
@@ -53,51 +50,29 @@ export default function Home() {
           <h1><IoIosArrowDown /></h1>
         </div>
 
-        <div className={styles.bottom_section}>
-          <div className={styles.examples_section}>
-            <h1 className={styles.header_text}>PROJECTS<a className={styles.white_dot}>.</a></h1>
-            <p className={styles.description_text}>Our previous & current projects.</p>
-            <div className={styles.header_seperator}></div>
+        <div className={styles.resolve_problems}>
+          <h1 className={styles.solve_once_text}>Resolve Problems, Don't Re-Solve.</h1>
+          <p className={styles.solve_once_description}>Problems should only have to be solved once and once only.</p>
+          <p className={styles.solve_once_description}>Our code is designed to last from the beginning.</p>
 
-            <div className={styles.inside_components}>
-              <div className={styles.solvio}>
-                <div className={styles.example_top_section}>
-                  <div className={styles.image_container}>
-                    <img src="solvio_app_icon.png" className={styles.solvio_app_icon}/>
-                  </div>
-                  <div className={styles.example_details}>
-                    <h1 className={styles.solvio_example_text}>SOLVIO.APP</h1>
-                    <p className={styles.default_medium_text}>Why should Problems have to be difficult?</p>
-                    <div className={styles.tags}>
-                      <div className={styles.tag}>
-                        <p className={styles.default_medium_text}>Expo</p>
-                      </div>
-                      <div className={styles.tag}>
-                        <p className={styles.default_medium_text}>NextJS</p>
-                      </div>
-                      <div className={styles.tag}>
-                        <p className={styles.default_medium_text}>Typescript</p>
-                      </div>
-                      <div className={styles.tag}>
-                        <p className={styles.default_medium_text}>GoLang</p>
-                      </div>
-                      <div className={styles.tag}>
-                        <p className={styles.default_medium_text}>Postgres</p>
-                      </div>
-                    </div>
-                    <div className={styles.example_section_buttons}>
-                      <a className={styles.solvio_view_website_button} href='https://solvio.app/' target='_blank'>
-                        <p className={styles.a_tag_text}>View Website</p>
-                      </a>
-                      <div className={styles.button_seperator}></div>
-                      <a className={styles.solvio_other_button} href='https://github.com/GetSolvio' target='_blank'>
-                        <p className={styles.a_tag_text}>View Github</p>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <img className={styles.code_editor_image} src="code_editor.png"/>
+        </div>
+
+        <div className={styles.contact_us}>
+          <h1 className={styles.contact_us_text}>Need to Contact Us?</h1>
+          <p className={styles.contact_us_description}>Have a very specific query or even just a quick question?</p>
+          <p className={styles.contact_us_description}>feel free to email us.</p>
+
+          <div className={styles.email_button_us_container}>
+            <a className={styles.email_us_button} href="mailto:hello@resolvo.dev">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.at_icon}>
+                <path d="M21 12C21 10.1195 20.411 8.28625 19.3156 6.7577C18.2203 5.22915 16.6736 4.08209 14.893 3.47763C13.1123 2.87316 11.187 2.84166 9.38744 3.38754C7.58792 3.93341 6.00459 5.02925 4.85982 6.52115C3.71505 8.01304 3.06635 9.82604 3.00482 11.7055C2.94329 13.585 3.47203 15.4366 4.51677 17.0001C5.56152 18.5637 7.06979 19.7608 8.82975 20.4232C10.5897 21.0856 12.513 21.18 14.3294 20.6933" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/>
+                <circle cx="12" cy="12" r="4" stroke="#FFFFFF" stroke-width="2"/>
+                <path d="M16 9V13.5C16 14.8807 17.1193 16 18.5 16V16C19.8807 16 21 14.8807 21 13.5V12" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/>
+              </svg>
+
+              Email Us
+            </a>
           </div>
         </div>
       </div>
